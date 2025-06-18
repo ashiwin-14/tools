2dd34e70a4f9fe7c5cf70d3dcc181bcc91cba2e5
ashiwin-14 <azmovies261@gmail.com>
2025-06-18 21:56:03 +0530

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tools/',
+  base: 'https://ashiwin-14.github.io/tools/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
